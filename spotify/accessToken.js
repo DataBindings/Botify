b79e36eb1b36e656c17c75706bb8d4b47069844f
Jeremie Pedden <59879119+jeremiepedden@users.noreply.github.com>
2023-10-05 12:41:05 -0500

    Refactor: function now throws errors

--- spotify/accessToken.js
+++ spotify/accessToken.js
@@ -2,46 +2,61 @@ import querystring from "node:querystring";
 import 'dotenv/config';
 
 /**
- * Retrieves a Spotify access token using the provided refresh token.
+ * Retrieves a Spotify access token using a client's credentials and a refresh token.
  *
- * This function sends a POST request to the Spotify Accounts service to obtain
- * an access token using the provided refresh token and client credentials.
+ * This function makes a POST request to the Spotify Accounts API to obtain a new access token
+ * using the provided client ID, client secret, and refresh token. It handles errors and returns
+ * the access token data if successful.
  *
- * @async
- * @function  getAccessToken
- * @throws    {Error} If there's an issue with the network request or the response.
- * @returns   {Promise<object>} A Promise that resolves to an object containing the access token.
+ * @param {string} clientId - The client ID obtained from your Spotify developer dashboard.
+ * @param {string} clientSecret - The client secret obtained from your Spotify developer dashboard.
+ * @param {string} refreshToken - The refresh token associated with the user's Spotify account.
+ *
+ * @returns {Promise<Object>} A Promise that resolves to an object containing the access token data.
+ * @throws {Error} Throws an error with an error message if the token retrieval fails.
  */
 const getAccessToken = async (clientId, clientSecret, refreshToken) => {
-  try {
+  // Spotify Accounts API endpoint URL
+  const url = `https://accounts.spotify.com/api/token`;
 
-    // Spotify Accounts API endpoint URL
-    const url = `https://accounts.spotify.com/api/token`;
+  // Create a Base64-encoded "Basic" authorization header
+  const basic = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
 
-    // Create a Base64-encoded "Basic" authorization header
-    const basic = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+  // Make an HTTP POST request to obtain a new access token
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+    }),
+  });
 
-    // Make an HTTP POST request to obtain a new access token
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        Authorization: `Basic ${basic}`,
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: querystring.stringify({
-        grant_type: 'refresh_token',
-        refresh_token: refreshToken,
-      }),
-    });
-
-    // Ensure the response is in JSON format and return the parsed JSON data
-    const responseData = await response.json();
-
-    return responseData;
-  } 
-  catch (error) {
-    throw new Error(`Failed to retrieve Spotify access token: ${error.message}`);
+  // Ensure the response is valid; otherwise, throw errors
+  if (!response.ok) {
+    throw new Error("Failed to retrieve Spotify access token: Network Error");
   }
-};
+
+  // Ensure the response is in JSON format and return the parsed JSON data
+  const responseData = await response.json();
+
+  // Handle specific error cases and throw errors with corresponding messages
+  if (responseData.error_description === "Invalid client") {
+    throw new Error("Failed to retrieve Spotify access token: incorrect client id");
+  }
+
+  if (responseData.error_description === "Invalid client secret") {
+    throw new Error("Failed to retrieve Spotify access token: incorrect client secret");
+  }
+
+  if (responseData.error_description === "Invalid refresh token") {
+    throw new Error("Failed to retrieve Spotify access token: incorrect refresh token");
+  }
+
+  return responseData;
+}
 
 export default getAccessToken;
